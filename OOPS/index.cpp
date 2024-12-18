8f4d821b7151de1f36e02a8bb733d373e6f0b421
Rakshit412005 <rk1056@srmist.edu.in>
2024-12-18 17:35:48 +0530

    OOPS

---
+++ OOPS/index.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ class hero{
+   public:
+    int age;
+    string name;
+ };
+ int main(){
+    hero r;
+    r.age=19;
+    r.name="rakshit";
+    cout<<r.name<<endl;
+    cout<<r.age<<endl;
+
+    return 0;
+ }
