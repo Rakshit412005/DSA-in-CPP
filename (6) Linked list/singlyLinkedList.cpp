e5734cad6e4d752e502b50720dcc8669c57707a2
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-26 23:26:44 +0530

    //

--- (6) Linked list/singlyLinkedList.cpp
+++ (6) Linked list/singlyLinkedList.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public : 
+class Node
+{
+public:
     int data;
-    Node* next;
-    Node(int data){
+    Node *next;
+    Node(int data)
+    {
         this->data = data;
         this->next = NULL;
     };
-
 };
+
+void instertAtHead(Node *&Head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = Head;
+    Head = temp;
+};
+
+void print(Node *&Head)
+{
+    Node *temp = Head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+    Node *Head = node1;
+    instertAtHead(Head, 9);
+    print(Head);
+    return 0;
+}
