0c5bf555f49df6f4c55ca69e37c067e77db910ed
Rakshit412005 <rk1056@srmist.edu.in>
2025-08-26 23:57:02 +0530

    ..

--- (6) Linked list/singlyLinkedList.cpp
+++ (6) Linked list/singlyLinkedList.cpp
@@ -13,12 +13,35 @@ public:
     };
 };
 
-void instertAtHead(Node *&Head, int d)
+void instertAtHead(Node* &Head, int d)
 {
     Node *temp = new Node(d);
     temp->next = Head;
     Head = temp;
 };
+
+void insertAtTail(Node* &Tail, int d){
+    Node *temp = new Node(d);
+    Tail->next = temp;
+    Tail = temp;
+}
+
+void insertAtPosition(Node* &Head,int position, int d){
+
+    if(position==1){
+        instertAtHead(Head,d);
+        return;
+    }
+
+    Node *temp = Head;
+    int count = 1;
+    while (count < position -1){
+        temp = temp->next;
+    }
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
 
 void print(Node *&Head)
 {
@@ -28,13 +51,19 @@ void print(Node *&Head)
         cout << temp->data << " ";
         temp = temp->next;
     }
+    cout<<endl;
 }
 
 int main()
 {
     Node *node1 = new Node(10);
     Node *Head = node1;
+    Node *Tail = node1;
     instertAtHead(Head, 9);
+    print(Head);
+    insertAtTail(Tail,11);
+    print(Head);
+    insertAtPosition(Head,2,12);
     print(Head);
     return 0;
 }
