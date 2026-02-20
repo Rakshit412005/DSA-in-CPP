336e6cc4b4c310c241b6dc8543190f2fd6370552
Rakshit412005 <rk1056@srmist.edu.in>
2026-02-20 10:36:18 +0530

    best time to buy and sell a stock (Leetcode)

---
+++ Arrays/buyNsellStock.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+
+
+    int maxProfit(vector<int>& prices) {
+       int maxProfit = 0;
+       int n = prices.size();
+       int min = INT_MAX;
+       
+       for(int i = 0; i<n; i++){
+        if(prices[i]<min){
+            min = prices[i];
+        }
+        int profit = prices[i]-min;
+        if(profit>maxProfit){
+            maxProfit = profit;
+        }
+
+       }
+       return maxProfit;
+    }
+};
