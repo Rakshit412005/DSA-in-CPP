e341a88fad8e9509e3f3c31e2ea9cbf414e313db
Rakshit412005 <rk1056@srmist.edu.in>
2026-06-07 14:49:14 +0530

    Merge Sorted Array Leetcode - 88

---
+++ SDE sheet/mergeSortedArray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;  
+
+// leetcode solution for merge sorted array problem
+class Solution {// copy from here
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n -1;
+
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
+        
+    }
+}; // compare last valid nos of num1 and num 2  and insert from the last of num1
