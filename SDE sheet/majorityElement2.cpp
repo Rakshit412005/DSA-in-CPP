58014af7d2a5ff0ca0183a507e8552277941af02
Rakshit412005 <rk1056@srmist.edu.in>
2026-06-10 22:04:06 +0530

    Majority Element 2 - Leetcode 229

---
+++ SDE sheet/majorityElement2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//leetcode solution for majority element 2
+class Solution {//copy from here
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0;
+        int cand1 = 0, cand2 = 1;
+
+        for (int num : nums) {
+            if (num == cand1) {
+                cnt1++;
+            } else if (num == cand2) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                cand1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                cand2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = cnt2 = 0;
+
+        for (int num : nums) {
+            if (num == cand1) cnt1++;
+            else if (num == cand2) cnt2++;
+        }
+
+        vector<int> ans;
+        int n = nums.size();
+
+        if (cnt1 > n / 3) ans.push_back(cand1);
+        if (cnt2 > n / 3) ans.push_back(cand2);
+
+        return ans;
+    }
+};
