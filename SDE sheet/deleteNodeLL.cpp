77218b133bbfad5054a8f36662b5b991e2044d66
Rakshit412005 <rk1056@srmist.edu.in>
2026-06-14 21:56:04 +0530

    Delete Node in a Linked List - Leetcode 237

---
+++ SDE sheet/deleteNodeLL.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//leetcode solution for deleting a node in a linked list given only access to that node
+class Solution {// copy from here
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
