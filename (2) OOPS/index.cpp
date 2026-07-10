8ea64fd8a315533034e4b10ed9aeb520058b0858
Rakshit412005 <rk1056@srmist.edu.in>
2026-07-10 22:24:33 +0530

    OOPS Practice

--- (2) OOPS/index.cpp
+++ (2) OOPS/index.cpp
@@ -1,16 +1,50 @@
 #include<iostream>
 using namespace std;
- class hero{
-   public:
-    int age;
-    string name;
- };
- int main(){
-    hero r;
-    r.age=19;
-    r.name="rakshit";
-    cout<<r.name<<endl;
-    cout<<r.age<<endl;
 
-    return 0;
- }
+class Student{
+   public:
+
+   int id;
+   string name;
+   int age;
+   int rollno;
+
+   Student(int id, string name, int age, int rollno){
+      
+      this->id = id;
+      this->name = name;
+      this->age = age;
+      this->rollno = rollno;
+
+      cout<<this->name<<" Student instance created"<<endl;
+   }
+
+   void study(){
+      cout<<this->name<<" Studying"<<endl;
+   }
+
+   void bunk(){
+      cout<<this->name<<" Bunking"<<endl;
+   }
+
+   void sleep(){
+      cout<<this->name<<" Sleeping"<<endl;
+   }
+};
+
+int main(){
+
+   Student A(1,"Rakshit",21,755);
+
+   cout<<A.name<<endl;
+   cout<<A.id<<endl;
+   cout<<A.rollno<<endl;
+   cout<<A.age<<endl;
+
+   A.study();
+   A.bunk();
+   A.sleep();
+
+   return 0;
+
+}
